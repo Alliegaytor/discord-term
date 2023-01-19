0d0a32c9a2d4348cdddc3a527cd487a8ed1073e4
Allison Smith <allisondaniellesmith@protonmail.com>
2023-01-20 02:18:21 +1100

    Create permissionCheck() to check for permissions

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { Guild, TextChannel, Channel, GuildChannel, Permissions } from "discord.js";
+import { UserId } from "./types.js";
 
 export default abstract class Utils {
     public static findDefaultChannel(guild: Guild): TextChannel {
@@ -21,5 +22,24 @@ export default abstract class Utils {
 
     public static getRandomInt(min: number, max: number): number {
         return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    // Checks to see if client has permissions on a channel in an array
+    public static permissionCheck(channel: TextChannel, userid: UserId, permissions: Array<bigint> ): Array<boolean> {
+        const userperms = channel.permissionsFor(userid);
+        const results: Array<boolean> = new Array(permissions.length);
+
+        let i: number = 0;
+        permissions.forEach(permission => {
+            if (userperms?.has(permission, true)) {
+                results[i] = true;
+            }
+            else {
+                results[i] = false;
+            }
+            i+=1
+        })
+
+        return results;
     }
 }
