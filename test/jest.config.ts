78a9ad9275997c6c683d882c8b38bcdc8f9cb2d1
Allison Smith <allisondaniellesmith@protonmail.com>
2023-02-02 00:08:53 +1100

    Testing 🥳

---
+++ test/jest.config.ts
@@ -0,0 +1,9 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
+    preset: "ts-jest/presets/default-esm",
+    testEnvironment: "node",
+    extensionsToTreatAsEsm: [".ts"],
+}
+
+export default jestConfig
